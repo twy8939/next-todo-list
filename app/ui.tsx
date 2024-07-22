8a9de69e419132e95943bd5db1f07553b3144815
twy8939 <twy8939@naver.com>
2024-07-23 07:59:34 +0900

    feat : UI > Todo List SearchBar Add

--- app/ui.tsx
+++ app/ui.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import { Input } from "@material-tailwind/react";
+
 export default function UI() {
   return (
-    <div className="w-2/3 mx-auto flex flex-col items-center py-10">
+    <div className="w-2/3 mx-auto flex flex-col items-center py-10 gap-2">
       <h1 className="text-xl">TODO LIST</h1>
+      <Input
+        label="Search TODO"
+        placeholder="Search TODO"
+        icon={<i className="fas fa-search" />}
+      />
     </div>
   );
 }
