7112f87cd28b03ab0e1e895dbe66a0aa990a8fc3
twy8939 <twy8939@naver.com>
2024-07-22 23:05:22 +0900

    feat : UI > Todo List Title Add

---
+++ app/ui.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function UI() {
+  return (
+    <div className="w-2/3 mx-auto flex flex-col items-center py-10">
+      <h1 className="text-xl">TODO LIST</h1>
+    </div>
+  );
+}
